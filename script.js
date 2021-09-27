149070e324d00652619297bb318377480c4b646e
Jeff Giuliano <jgiuliano8@yahoo.com>
2021-09-26 20:06:08 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+//The display element
+const displayElem = document.getElementById("display");
+
+//Memory button elements
+const btnMemAdd = document.getElementById("memory-add");
+const btnMemSub = document.getElementById("memory-sub");
+const btnMemRecClr = document.getElementById("memory-recall-clear");
+const btnClrDisp = document.getElementById("clear-display");
+
+//Number button elements
+const btnNumDblZero = document.getElementById("dbl-zero");
+const btnNumZero = document.getElementById("zero");
+const btnNumOne = document.getElementById("one");
+const btnNumTwo = document.getElementById("two");
+const btnNumThree = document.getElementById("three");
+const btnNumFour = document.getElementById("four");
+const btnNumFive = document.getElementById("five");
+const btnNumSix = document.getElementById("six");
+const btnNumSeven = document.getElementById("seven");
+const btnNumEight = document.getElementById("eight");
+const btnNumNine = document.getElementById("nine");
+
+//Function button elements
+const btnFuncDiv = document.getElementById("division");
+const btnFuncMult = document.getElementById("multiplication");
+const btnFuncSqrt = document.getElementById("sqrt");
+const btnFuncPerc = document.getElementById("percent");
+const btnFuncMin = document.getElementById("minus");
+const btnFuncPosNeg = document.getElementById("pos-neg");
+const btnFuncPlus = document.getElementById("plus");
+const btnFuncEql = document.getElementById("equals");
